122aac72666b0991120a1e76ef0870492968f7d2
S0UMIL <soumil.tiwary@gmail.com>
2025-09-02 19:05:36 +0530

    Todo list.py

---
+++ Todo list.py
@@ -0,0 +1,4 @@
+Todo = ['Get quarters', 'Do laundry','Take a walk','Get a haircut','Make some tea','Complete lists chapter','call mom','watch my hero academia']
+print(Todo[0])
+print(Todo[1])
+print(Todo[2 : 4])
