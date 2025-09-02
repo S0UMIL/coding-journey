acd6d56201324205400f0ad0632742ce17c7fade
S0UMIL <soumil.tiwary@gmail.com>
2025-09-02 19:16:28 +0530

    Create PokeDex.py

---
+++ PokeDex.py
@@ -0,0 +1,26 @@
+class Pokemon:
+  def __init__(self, entry , name , types ,description , is_caught):
+    self.entry = entry
+    self.name = name
+    self.types = types
+    self.description = description
+    self.is_caught = is_caught
+Pikachu = Pokemon(25,'Pikachu','Electric','Description:It has small electric sacs on both its cheeks. If threatened, it looses electric charges from the sacs.' ,'Pikachu has already been caught')
+def speak(self):
+  print(self.name,self.name)
+  return(self.name)
+def display_details(self):
+  print(self.entry)
+  print(self.name)
+  print(self.types)
+  print(self.description)
+  print(self.is_caught)
+  return(self)
+speak(Pikachu)
+display_details(Pikachu)
+Squirtle = Pokemon(26, 'Squirtle','Water','Description: After birth, its back swells and hardens into a shell. It sprays a potent foam from its mouth.','Squirtle has not been caught!' )
+Charmandar = Pokemon(27 , 'Charmandar' ,'Fire','Description: The flame on its tail shows the strength of its life-force. If Charmander is weak, the flame also burns weakly.','Charmandar has already been caught')
+speak(Squirtle)
+display_details(Squirtle)
+speak(Charmandar)
+display_details(Charmandar)
