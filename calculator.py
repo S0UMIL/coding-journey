9efb9e3bf33198b261c522858551ef5058cb429d
S0UMIL <soumil.tiwary@gmail.com>
2025-09-02 19:11:23 +0530

    Create calculator.py
    made a working calculator using functions

---
+++ calculator.py
@@ -0,0 +1,22 @@
+x=int(input('your 1st num: '))
+y=int(input('your 2nd num: '))
+def add(x,y):
+  answer4 = x + y
+  print (answer4)
+  return answer4
+def sub(x,y):
+  answer3 = x - y
+  print(answer3)
+  return answer3
+def multiplication(x,y):
+  answer2 = x * y
+  print (answer2)
+  return answer2
+def div(x,y):
+  answer1 = x / y
+  print(answer1)
+  return answer1
+add(x,y)
+sub(x,y)
+multiplication(x,y)
+div(x,y)
