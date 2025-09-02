e008db2c94df18eb5b728b5d8c99156de29c128e
S0UMIL <soumil.tiwary@gmail.com>
2025-09-02 19:08:13 +0530

    bucket_list.py

---
+++ bucket_list.py
@@ -0,0 +1,3 @@
+things_to_do = ['learn coding' ,'improve my overall physical health and fitness','spend more time talking to family and ppl areound me','interact with more people ']
+for i in things_to_do:
+  print(i)
