7c0c1c87a563fd3e5a584050a4015e79c1a89ebd
Soumil Tiwary <soumil.tiwary@gmail.com>
2026-02-02 00:28:29 +0530

    Modify reddit variable assignments in learningVariables.cpp
    Adjust variable assignments for clarity and consistency.

---
+++ learningVariables.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  
+  double reddit=34;
+  reddit=180.27;
+  reddit=229.87;
+  reddit=216.47;
+  reddit=208.95;
+  reddit=229.99;
+  reddit=225.08;
+  printf("%lf", reddit);
+}
